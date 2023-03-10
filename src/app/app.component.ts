95fbf8e5893ae354fe6d8d38bdd07bd7fa444d79
stefania <stefaniazocco93@gmail.com>
2023-03-10 12:44:14 +0100

    aggiunte robe boh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { STUDENTS } from './data/students';
 import { Hobby } from './models/hobby';
 import { Language } from './models/language';
@@ -13,33 +14,46 @@ import { StudentHttpService } from './providers/services/student-http.service';
 export class AppComponent {
   title = 'movie-app';
   students = structuredClone(STUDENTS);
-  selectedStudent: Student |undefined;
+  selectedStudent: Student | undefined;
   mode: 'edit' | 'add' = 'edit'
 
-  constructor(private readonly studentHttpService: StudentHttpService){
-    this.studentHttpService.getStudents();
+  studentsFromSerevice!: Array<Student>;
+  studentsSubscription!: Subscription;
+  students$!: Observable<Array<Student>>;
+
+  constructor(private readonly studentHttpService: StudentHttpService) {
+    // this.students$!= this.studentHttpService.getStudents(); //poi nell'html si scrive  students$ | async (per più observable meglio fare una util come l'unsubscribe)
+    // this.studentHttpService.getStudents();
+    this.studentHttpService.getStudents()
+      .subscribe({
+        next: (students: Array<Student>) => this.studentsFromSerevice = students
+      })
   }
+  // ngOnDestroy(): void {
+  //   if (this.studentsSubscription) {
+  //     this.studentsSubscription.unsubscribe()
+  //   }
+  // }
 
-
-  addStudent():void{
+  addStudent(): void {
     this.selectedStudent = new Student();
     this.mode = 'add';
   }
-  
-  handleAddStudent(newStudent: Student):void{
+
+  handleAddStudent(newStudent: Student): void {
     this.students.unshift(newStudent);
-    this.selectedStudent= undefined;
+    this.selectedStudent = undefined;
   }
   editStudent(student: Student): void {
     this.selectedStudent = student;
     this.mode = 'edit';
   }
-  handleEditStudent(student: Student): void{
-    const studentIndex: number= this.students.findIndex((st: Student): boolean => st.nome === student.nome && st.cognome === student.cognome)
-    if(studentIndex >= 0) {
+  handleEditStudent(student: Student): void {
+    const studentIndex: number = this.students.findIndex((st: Student): boolean => st.nome === student.nome && st.cognome === student.cognome)
+    if (studentIndex >= 0) {
       this.students.splice(studentIndex, 1, student);
-    } 
-    this.selectedStudent= undefined;
+    }
+    this.selectedStudent = undefined;
   }
 
 }
--- src/app/providers/services/student-http.service.ts
+++ src/app/providers/services/student-http.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs";
+import { Student } from "src/app/models/student";
 
 
 @Injectable({providedIn:'root'})
@@ -7,10 +9,16 @@ export class StudentHttpService{
     constructor(private readonly http: HttpClient){}
 
     getStudents(){
-        return this.http.get('../../assets/students.json', {responseType:'json'})
-        .subscribe({
-            next:(value) => console.table(value),
-            error:(error: Error)=> console.error(error)
-        })
+        //get ritorna un observable quindi si può fare la sottoscrizione
+        //il pipe permette di eserguire codice sugli eventi o valori emessi da un observable 
+        //prima di raggiungere gli observers sottoscritti
+        return this.http.get<Array<Student>>('../../assets/students.json', {responseType:'json'})
+        .pipe(
+            map((students: Array<Student>)=> students.map(stud => new Student(stud)))
+        )
+        // .subscribe({
+        //     next:(value) => console.table(value),
+        //     error:(error: Error)=> console.error(error)
+        // })
     }
 }
